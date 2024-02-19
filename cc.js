457b9e87ae383decf6b515812143c8b921b36b99
Mahathir Mohammad <156532540+mahathir-f@users.noreply.github.com>
2024-02-19 08:17:43 -0800

    final commit

---
+++ cc.js
@@ -0,0 +1,49 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+  const dropdown = document.querySelectorAll(".dropdown select");
+  const btn = document.querySelector("form button");
+  const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg")
+  //let countryList = document.querySelector("countryList")
+  for(let slect of dropdown){
+    for ( currCode in countryList){
+        let newOption = document.createElement("option")
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if(slect.name==="from" && currCode==="USD"){
+         newOption.selected="slected"
+        }else if(slect.name==="to" && currCode==="BDT"){
+            newOption.selected="slected"
+           }
+        slect.append(newOption)
+        slect.addEventListener("change", (evt) =>{
+            updateFlag (evt.target)
+        })
+      }
+  }
+  
+ let updateFlag= (element)=>{
+      let currCode = element.value;
+     let countryCode = countryList[currCode];
+     let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+     let img = element.parentElement.querySelector("img")
+     img.src = newSrc;
+ }
+ btn.addEventListener("click", async(evt)=>{
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtval = amount.value;
+  if(amtval===""||amtval<1){
+    amtval = 1;
+    amount.value ="1";
+  }
+  //console.log(fromCurr.value,toCurr.value);
+  const URL =`${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[toCurr.value.toLowerCase()]
+  console.log(rate);
+  let finalRate = amtval*rate;
+  msg.innerText = `${amtval}${fromCurr.value}=${finalRate}${toCurr.value}`
+ })
